b4cc5fa1df3b4fd411abcca2e3907484cd11d182
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-05-24 10:34:09 +0200

    heroku connect

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>
-      <img src="../assets/progressLogo.png">
-      HEADER HERE
-    </div>
+  <div>
+    <img src="../assets/progressLogo.png">
+    HEADER HERE SEE OR NOT
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "header"
-    }
+  export default {
+    name: "header"
+  }
 </script>
 
 <style scoped>
 
   img {
-    height:30px
+    height: 30px
   }
 </style>
